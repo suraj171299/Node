6ab1f8ad1df5587be34afc0678aac836bb3d3d78
suraj171299 <surajpadalkar1999@gmail.com>
2023-09-01 10:25:04 +0530

    Arrow fn and student obj

---
+++ student-object.js
@@ -0,0 +1,11 @@
+const student = {
+    id: 32,
+    name: 'Suraj',
+    age: 23,
+    university: 'Jspm University'
+}
+
+console.log(student.name);
+console.log(student.age);
+console.log(student.id);
+console.log(student.university);
