5be75bda4ad7fce71c190dab01021bcb75faa385
suraj171299 <surajpadalkar1999@gmail.com>
2023-09-02 20:01:34 +0530

    Read and write Project

--- NodeServer.js
+++ NodeServer.js
@@ -1,15 +1,23 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
     if (url === "/") {
-        res.write("<html>");
-        res.write("<head><title>Enter Message</title></head>");
-        res.write(
-            "<body><form action='/message' method='POST'><input type='text' name='message'><button type = 'submit'>Send</button></form></body>"
-        );
-        res.write("</html>");
-        return res.end();
+        fs.readFile("message.txt", (error, data) => {
+            if (error) {
+                console.log(error);
+            }
+            res.write("<html>");
+            res.write("<head><title>Enter Message</title></head>");
+            res.write(`<body>${data}</body>`)
+            res.write(
+                "<body><form action='/message' method='POST'><input type='text' name='message'><button type = 'submit'>Send</button></form></body>"
+            );
+            res.write("</html>");
+            return res.end();
+        });
     } else if (url === "/home") {
         res.setHeader("Content-Type", "text/html");
         res.write("<html>");
@@ -31,6 +39,21 @@ const server = http.createServer((req, res) => {
         res.write("<body><h1>WELCOME TO MY NODE JS PROJECT</h1></body>");
         res.write("</html>");
         return res.end();
+    } else if (url === "/message" && method === "POST") {
+        const body = [];
+        req.on("data", (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on("end", () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split("=")[1];
+            fs.writeFile("message.txt", message, (err) => {
+                res.statusCode = 302;
+                res.setHeader("Location", "/");
+                return res.end();
+            });
+        });
     }
     // res.setHeader("Content-Type", "text/html");
     // res.write("<html>");
